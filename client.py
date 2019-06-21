e0f8dce90d6d30b45778f9840526cb62bc5913b2
lukaszwoznica <lukaszlwoznica@gmail.com>
2019-06-21 23:21:51 +0200

    Commit

--- client.py
+++ client.py
@@ -1,79 +1,296 @@
+import ntpath
+import os
 import socket
+import zipfile
+import getpass
 
-errors_list = \
-    ["The server rejected the file because its name contains illegal characters",
-     "The server has rejected the file because its size is too large",
-     "The server rejected the file because it is not a ZIP file.",
-     "The server didn't receive the file data",
-     "An error occurred while saving the file to the server"]
+from colorama import Fore, Back
+
+BUFF = 4096
+
+errors_dict = \
+    {"101": "The server rejected the file because its name contains illegal characters!",
+     "102": "The server has rejected the file because its size is too large!",
+     "103": "The server rejected the file because it is not a ZIP file!",
+     "201": "The server didn't receive the file data!",
+     "202": "An error occurred while saving the file to the server!",
+     "301": "An invalid file path has been provided or the file does not exist!"}
+
+files_list = ""
 
 
-def uploadRequest(filename, filesize):
+def ipVersion(ip_addr):
+    try:
+        ip_addr = socket.gethostbyname(ip_addr)
+    except socket.error:
+        pass
+    try:
+        ip_addr = socket.getaddrinfo(ip_addr, None, socket.AF_INET6)[0][4][0]
+    except socket.error:
+        pass
+    try:
+        socket.inet_aton(ip_addr)
+        return 4
+    except socket.error:
+        pass
+    try:
+        socket.inet_pton(socket.AF_INET6, ip_addr)
+        return 6
+    except socket.error:
+        pass
+    return 1
+
+
+def renameIfExist(download_path, file_name):
+    counter = 1
+    filename_without_extension, file_extension = os.path.splitext(file_name)
+
+    while os.path.exists(download_path + file_name):
+        if counter == 1:
+            file_name = filename_without_extension
+        else:
+            if counter % 10 != 0:
+                del_chars = (3 + len(file_extension)) + int(len(str(counter)))
+            else:
+                del_chars = ((3 + len(file_extension)) + int(len(str(counter)))) - 1
+
+            file_name = file_name[:-del_chars]
+
+        file_name = file_name + " (" + str(counter) + ")" + file_extension
+        counter += 1
+
+    return file_name
+
+
+def uploadRequest(filename, filesize, sock):
     request = "UPLOAD\r\n" + \
               "FILENAME: " + filename + "\r\n" + \
               "FILESIZE: " + str(filesize) + "\r\n\r\n"
 
-    sock.send(request.encode())
+    sock.send(request.encode('utf8'))
 
     server_data = b''
-    while b'\r\n\r\n' not in server_data:
-        server_data += sock.recv(1)
-    response = server_data.decode().split(": ")
+    try:
+        while b'\r\n\r\n' not in server_data:
+            server_data += sock.recv(BUFF)
+    except socket.error as socket_exc:
+        print(Fore.RED + "Socket error: {0}".format(socket_exc))
+        return False
+
+    response = server_data.decode('utf8').split(": ")
 
     if "100" in response[1]:
         sock.send(file_content)
     elif "101" in response[1]:
-        print("Error: " + errors_list[0])
+        print(Fore.RED + "Error: " + errors_dict["101"])
         return False
     elif "102" in response[1]:
-        print("Error: " + errors_list[1])
+        print(Fore.RED + "Error: " + errors_dict["102"])
         return False
     elif "103" in response[1]:
-        print("Error: " + errors_list[2])
+        print(Fore.RED + "Error: " + errors_dict["103"])
         return False
 
     server_data = b''
-    while b'\r\n\r\n' not in server_data:
-        server_data += sock.recv(1)
-    response = server_data.decode().split(": ")
+    try:
+        while b'\r\n\r\n' not in server_data:
+            server_data += sock.recv(BUFF)
+    except socket.error as socket_exc:
+        print(Fore.RED + "Socket error: {0}".format(socket_exc))
+        return False
+
+    response = server_data.decode('utf8').split(": ")
+
+    if "204" in response[1]:
+        print(Fore.YELLOW + "A selected file is encrypted")
+        while True:
+            password = input(Fore.CYAN + "Enter password: ")
+            request = "PASSWORD: " + password + "\r\n\r\n"
+            sock.send(request.encode("utf-8"))
+            server_data = b''
+            try:
+                while b'\r\n\r\n' not in server_data:
+                    server_data += sock.recv(BUFF)
+            except socket.error as socket_exc:
+                print(Fore.RED + "Socket error: {0}".format(socket_exc))
+                return False
+            response = server_data.decode('utf8').split(": ")
+            if "204" in response[1]:
+                print(Fore.RED + "Invalid password!")
+                continue
+            elif "200":
+                break
 
     if "200" in response[1]:
-        print("The file has been successfully uploaded!")
+        request = "FILESLIST_REQUEST\r\n\r\n"
+        sock.send(request.encode('utf8'))
+        server_data = b''
+        while b'\r\n\r\n' not in server_data:
+            server_data += sock.recv(BUFF)
+        if server_data is None:
+            return False
+        global files_list
+        files_list = server_data.decode('utf8')[:-4]
+        print(
+            "\n" + Back.LIGHTBLACK_EX + "--------------------------------------------------------------------------------" + Back.RESET)
+        print(
+            Back.LIGHTBLACK_EX + "||" + Back.RESET + Fore.YELLOW + "                           Content of ZIP file                              " + Fore.RESET + Back.LIGHTBLACK_EX + "||" + Back.RESET)
+        print(
+            Back.LIGHTBLACK_EX + "--------------------------------------------------------------------------------" + Back.RESET)
+        print(files_list)
         return True
     elif "201" in response[1]:
-        print("Error: " + errors_list[3])
+        print(Fore.RED + "Error: " + errors_dict["201"])
     elif "202" in response[1]:
-        print("Error: " + errors_list[4])
+        print(Fore.RED + "Error: " + errors_dict["202"])
+    elif "203" in response[1]:
+        print(Fore.RED + "Error: " + errors_dict["203"])
 
     return False
 
-def downloadRequest(path):
+
+def downloadRequest(path, sock, download_path):
+    file_name = ntpath.basename(path)
+    if not download_path.endswith("/") or not download_path.endswith("\\"):
+        download_path = download_path + "/"
     request = "DOWNLOAD\r\n" + \
               "FILEPATH: " + path + "\r\n\r\n"
 
-    sock.send(request.encode())
+    sock.send(request.encode('utf8'))
+    server_data = b''
+
+    try:
+        while b'\r\n\r\n' not in server_data:
+            server_data += sock.recv(1)
+    except socket.error as socket_exc:
+        print(Fore.RED + "Socket error: {0}".format(socket_exc))
+        return
+
+    response = dict(
+        (k.lower(), v) for k, v in [i.split(': ') for i in server_data[:-4].decode('utf8').splitlines()])
+
+    file_size = int(response.get("filesize"))
+    if response.get("response") == "300" and file_size > 0:
+        file_content = b''
+
+        try:
+            while len(file_content) < file_size:
+                file_content += sock.recv(file_size - len(file_content))
+        except socket.error as socket_exc:
+            print(Fore.RED + "Socket error: {0}".format(socket_exc))
+            return
+
+        if not file_content:
+            print(Fore.RED + "\nAn error occurred while downloading the file from the server!")
+            return
+
+        if os.path.exists(download_path + file_name):
+            file_name = renameIfExist(download_path, file_name)
+
+        try:
+            file = open(download_path + file_name, "wb+")
+            file.write(file_content)
+            file.close()
+        except IOError as io_exc:
+            print(Fore.RED + "I/O error: {0}".format(io_exc))
+        else:
+            print(Fore.GREEN + "\nThe file has been successfully downloaded! :)")
+    elif response.get("response") == "301":
+        print(Fore.RED + "\nError: " + errors_dict["301"])
 
 
-server_address = ('127.0.0.1', 5000)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def quitRequest(sock):
+    request = "QUIT\r\n\r\n"
+    sock.send(request.encode('utf8'))
 
-try:
-    sock.connect(server_address)
-    file_name = input("Enter filename: ")
-    file = open(file_name, "rb")
-except socket.error as socket_exc:
-    print("Socket error: {0}".format(socket_exc))
-    exit(1)
-except IOError as io_exc:
-    print("I/O error: {0}".format(io_exc))
-    exit(1)
-else:
+
+if __name__ == "__main__":
+    server_address = ('127.0.0.1', 5000)
+    ip_ver = ipVersion(server_address[0])
+
+    if ip_ver == 4:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif ip_ver == 6:
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    else:
+        print("Invalid ip address!")
+        exit(1)
+
+    upload_result = False
+    app_title = """
+     ___________ _____    ______      _                  _             
+    |___  /_   _|  __ \  |  ____|    | |                | |            
+       / /  | | | |__) | | |__  __  _| |_ _ __ __ _  ___| |_ ___  _ __ 
+      / /   | | |  ___/  |  __| \ \/ / __| '__/ _` |/ __| __/ _ \| '__|
+     / /__ _| |_| |      | |____ >  <| |_| | | (_| | (__| || (_) | |   
+    /_____|_____|_|      |______/_/\_\\__|_|  \__,_|\___|\__\___/|_|        
+        """
+    current_download_path = os.path.dirname(os.path.realpath(__file__))
+    print(Fore.YELLOW + app_title + Fore.RESET)
+
+    try:
+        file_name = input(Fore.CYAN + "Enter path to ZIP file: " + Fore.RESET)
+        file = open(file_name, "rb")
+
+    except IOError as io_exc:
+        print(Fore.RED + "I/O error: {0}".format(io_exc))
+        exit(1)
+
     file_content = file.read()
     file_size = len(file_content)
     file.close()
-    upload_result = uploadRequest(file_name, file_size)
 
-if upload_result:
-    print("Download")
+    try:
+        sock.connect(server_address)
+    except socket.error as socket_exc:
+        print(Fore.RED + "Socket error: {0}".format(socket_exc))
+        exit(1)
 
+    upload_result = uploadRequest(file_name, file_size, sock)
 
+    if upload_result:
+        option = 0
+        while True:
+            option = input(Fore.YELLOW + "\nWhat do you want to do?\n" + Fore.RESET +
+                           "1. Download a file\n" +
+                           "2. Show content of ZIP file\n" +
+                           "3. Set the destination path for downloaded files (current: " + current_download_path + ")\n" +
+                           "4. Exit\n" +
+                           Fore.CYAN + "Your choice: " + Fore.RESET)
+            if option == "1":
+                file_path = input(Fore.CYAN + "\nEnter path to the file: " + Fore.RESET)
+                downloadRequest(file_path, sock, current_download_path)
+
+            elif option == "2":
+                print("\n" + Back.LIGHTBLACK_EX + "--------------------------------------------------------------------------------" + Back.RESET)
+                print(Back.LIGHTBLACK_EX + "||" + Back.RESET + Fore.YELLOW + "                           Content of ZIP file                              " + Fore.RESET + Back.LIGHTBLACK_EX + "||" + Back.RESET)
+                print(Back.LIGHTBLACK_EX + "--------------------------------------------------------------------------------" + Back.RESET)
+                print(Back.RESET + files_list)
+
+            elif option == "3":
+                new_path = input(Fore.CYAN + "Enter path: " + Fore.RESET)
+                while not os.path.exists(new_path):
+                    choice = input(Fore.CYAN + "\nThe path you entered does not exist. Do you want to create it? (y/n) " + Fore.RESET)
+
+                    if choice.lower() == "y" or choice.lower() == "yes":
+                        try:
+                            os.mkdir(new_path)
+                        except IOError as io_exc:
+                            print(Fore.RED + "I/O error: {0}".format(io_exc))
+
+                    elif choice.lower() == "n" or choice.lower() == "no":
+                        new_path = input(Fore.CYAN + "Enter path: " + Fore.RESET)
+
+                    else:
+                        print(Fore.RED + "Invalid choice!")
+                        continue
+
+                current_download_path = new_path
+
+            elif option == "4":
+                quitRequest(sock)
+                print(Fore.RESET + "\nConnection closed")
+                break
+
+            else:
+                print(Fore.RED + "\nInvalid choice!")
--- server.py
+++ server.py
@@ -3,86 +3,325 @@ import os
 import re
 import socket
 import threading
+import zipfile
+import io
+import sys
+import logging
+
+BUFF = 4096
+logging.basicConfig(filename='Logs/server_logs.log', filemode='a', format='%(asctime)s ; %(levelname)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+
+def ipVersion(ip_addr):
+    try:
+        ip_addr = socket.gethostbyname(ip_addr)
+    except socket.error:
+        pass
+    try:
+        ip_addr = socket.getaddrinfo(ip_addr, None, socket.AF_INET6)[0][4][0]
+    except socket.error:
+        pass
+    try:
+        socket.inet_aton(ip_addr)
+        return 4
+    except socket.error:
+        pass
+    try:
+        socket.inet_pton(socket.AF_INET6, ip_addr)
+        return 6
+    except socket.error:
+        pass
+    return 1
 
 
 def disconnectClient(conn, addr):
     conn.close()
-    print("Disconnected with: " + addr[0])
+    print("Disconnected with: " + addr[0] + ":" + str(addr[1]))
+    logging.info("Disconnected with: " + addr[0] + ":" + str(addr[1]))
+
+
+def checkFileConditions(file_name, file_size):
+    illegal_characters = re.compile('[/\:*?"<>|]')
+
+    if illegal_characters.search(file_name) is not None:
+        return 101
+
+    elif file_size > 524288000:
+        return 102
+
+    elif not file_name.endswith(".zip"):
+        return 103
+
+    return 100
+
+
+def saveFile(file_name, file_content):
+    counter = 1
+    file_name_temp = file_name
+    while os.path.exists("uploaded_files/" + file_name_temp):
+        if counter == 1:
+            file_name_temp = file_name_temp[:-4]
+        else:
+            if counter % 10 != 0:
+                del_chars = 7 + int(len(str(counter)))
+            else:
+                del_chars = (7 + int(len(str(counter)))) - 1
+
+            file_name_temp = file_name_temp[:-del_chars]
+
+        file_name_temp = file_name_temp + " (" + str(counter) + ")" + ".zip"
+        counter += 1
+
+    try:
+        file = open("uploaded_files/" + file_name_temp, "wb+")
+        file.write(file_content)
+        file.close()
+
+    except IOError as io_exc:
+        print("I/O error: {0}".format(io_exc))
+        logging.error("I/O error: {0}".format(io_exc))
+        return False
+
+    return file_name_temp
+
+
+def zipFileContent(file_name):
+    zip_file = zipfile.ZipFile("uploaded_files/" + file_name, "r")
+    old_stdout = sys.stdout
+    content = io.StringIO()
+    sys.stdout = content
+    zip_file.printdir()
+    sys.stdout = old_stdout
+    zip_file.close()
+    return content.getvalue()
+
+
+def checkFileExistence(file_path, zip_file):
+    file = zipfile.ZipFile("uploaded_files/" + zip_file, "r")
+    if file_path in file.namelist():
+        file.close()
+        return True
+    file.close()
+    return False
+
+
+def getFileFromZip(file_path, zip_file):
+    if checkFileExistence(file_path, zip_file):
+        file = zipfile.ZipFile("uploaded_files/" + zip_file, "r")
+        opened_file = file.open(file_path)
+        file.close()
+        file_content = opened_file.read()
+        file_size = len(file_content)
+        file_data = {"filesize": file_size,
+                     "filecontent": file_content}
+        return file_data
+
+    return False
 
 
 def oneClient(conn, addr):
     recived_data = b''
     while b'\r\n\r\n' not in recived_data:
-        recived_data += conn.recv(1)
+        recived_data += conn.recv(BUFF)
 
-    print(recived_data.decode())
-    if "upload" in recived_data.decode().splitlines()[0].lower():
-        file_data = dict((k.lower(), v) for k, v in [i.split(': ') for i in recived_data[:-4].decode().splitlines()[1:]])
+    # Check if client requested upload
+    if "upload" in recived_data.decode('utf8').splitlines()[0].lower():
+        file_data = dict(
+            (k.lower(), v) for k, v in [i.split(': ') for i in recived_data[:-4].decode('utf8').splitlines()[1:]])
         file_name = ntpath.basename(file_data.get('filename', "new_file"))
         file_size = int(file_data.get('filesize', 0))
-        regex = re.compile('[/\:*?"<>|]')
+        print(recived_data)
+        print(file_data)
+        print("Upload request form: " + addr[0] + ":" + str(addr[1]) + "\n" +
+              "File name: " + file_name + "\n" +
+              "File size: " + str(file_size) + " B")
+        logging.info("Upload request form: " + addr[0] + ":" + str(addr[1]) + "\n" +
+                     "File name: " + file_name + "\n" +
+                     "File size: " + str(file_size) + " B")
 
-        if regex.search(file_name) is not None:
-            response = "RESPONSE: 101\r\n\r\n"
-            conn.send(response.encode())
+        response_code = checkFileConditions(file_name, file_size)
+
+        if response_code is not 100:
+            response = "RESPONSE: " + str(response_code) + "\r\n\r\n"
+            print("Uploading failed!")
+            logging.error("Uploading failed!")
+            conn.send(response.encode('utf8'))
             disconnectClient(conn, addr)
             return
-        elif file_size > 50000000:
-            response = "RESPONSE: 102\r\n\r\n"
-            conn.send(response.encode())
+
+        response = "RESPONSE: 100\r\n\r\n"
+        conn.send(response.encode('utf8'))
+
+        if file_size <= 0:
             disconnectClient(conn, addr)
             return
-        elif not file_name.endswith(".zip"):
-            response = "RESPONSE: 103\r\n\r\n"
-            conn.send(response.encode())
+
+        # Getting file content
+        file_content = b''
+        while len(file_content) < file_size:
+            file_content += conn.recv(file_size - len(file_content))
+
+        if not file_content:
+            response = "RESPONSE: 201\r\n\r\n"
+            conn.send(response.encode('utf8'))
             disconnectClient(conn, addr)
             return
+
+        save_file_result = saveFile(file_name, file_content)
+        if not save_file_result:
+            response = "RESPONSE: 201\r\n\r\n"
+            conn.send(response.encode('utf8'))
+            disconnectClient(conn, addr)
+            return
+
+        temp_file_name = save_file_result
+        print("File " + file_name + " has been successfully saved on the server as " + temp_file_name)
+        logging.info("File " + file_name + " has been successfully saved on the server as " + temp_file_name)
+        is_encrypted = False
+
+        try:
+            test_zip = zipfile.ZipFile("uploaded_files/" + temp_file_name, "r")
+            test_zip.testzip()
+        except RuntimeError:
+            print("File " + temp_file_name + " is encrypted. A password is required to extraction.")
+            logging.info("File " + temp_file_name + " is encrypted. A password is required to extraction.")
+            is_encrypted = True
+            response = "RESPONSE: 204\r\n\r\n"
+            conn.send(response.encode('utf8'))
+        except Exception:
+            print("File " + temp_file_name + " is damaged")
+            logging.error("File " + temp_file_name + " is damaged")
+            response = "RESPONSE: 203\r\n\r\n"
+            conn.send(response.encode('utf8'))
+            disconnectClient(conn, addr)
+            os.remove("uploaded_files/" + temp_file_name)
+            return
+
+        if is_encrypted:
+            while True:
+                recived_data = b''
+                try:
+                    while b'\r\n\r\n' not in recived_data:
+                        recived_data += conn.recv(BUFF)
+                except socket.error as socket_exc:
+                    print("Socket error: {0}".format(socket_exc))
+                    logging.error("Socket error: {0}".format(socket_exc))
+
+                password = recived_data.decode('utf8').split(": ")
+
+                try:
+                    test = test_zip.infolist()
+                    test_open = test_zip.open(test[0].filename, "r", password[1][:-4].encode("utf-8"))
+                    test_open.close()
+                except IndexError as index_exc:
+                    print("Index error: {0}".format(index_exc))
+                    logging.error("Index error: {0}".format(index_exc))
+                except RuntimeError:
+                    print("Bad password for file: " + temp_file_name)
+                    logging.error("Bad password for file: " + temp_file_name)
+                    response = "RESPONSE: 204\r\n\r\n"
+                    conn.send(response.encode('utf8'))
+                    continue
+                print("Correct password for file: " + temp_file_name)
+                logging.error("Correct password for file: " + temp_file_name)
+                break
+
+        test_zip.close()
+        response = "RESPONSE: 200\r\n\r\n"
+        conn.send(response.encode('utf8'))
+
+        recived_data = b''
+        while b'\r\n\r\n' not in recived_data:
+            recived_data += conn.recv(BUFF)
+
+        if recived_data is not None and "fileslist_request" in recived_data.decode('utf8').lower():
+            response = zipFileContent(temp_file_name) + "\r\n\r\n"
+            conn.send(response.encode('utf8'))
         else:
-            response = "RESPONSE: 100\r\n\r\n"
-            conn.send(response.encode())
-            file_content = b''
-            if file_size > 0:
-                while len(file_content) < file_size:
-                    file_content += conn.recv(file_size - len(file_content))
+            disconnectClient(conn, addr)
+            return
 
-                if not file_content:
-                    response = "RESPONSE: 201\r\n\r\n"
-                    conn.send(response.encode())
-                    disconnectClient(conn, addr)
-                    return
+        # Waiting for a client requests
+        while True:
+            recived_data = b''
+            try:
+                while b'\r\n\r\n' not in recived_data:
+                    recived_data += conn.recv(BUFF)
+            except socket.error:
+                os.remove("uploaded_files/" + temp_file_name)
+                disconnectClient(conn, addr)
+                return
+
+            if "quit" in recived_data.decode('utf8').lower():
+                os.remove("uploaded_files/" + temp_file_name)
+                disconnectClient(conn, addr)
+                break
+
+            if "download" in recived_data.decode('utf8').splitlines()[0].lower():
+                download_request = dict((k.lower(), v) for k, v in
+                                        [i.split(': ') for i in recived_data[:-4].decode('utf8').splitlines()[1:]])
+                file_path = download_request.get('filepath')
+                print("Download request form: " + addr[0] + ":" + str(addr[1]) + "\n" +
+                      "File: " + temp_file_name[:-4] + "/" + file_path + "\n")
+                logging.info("Download request form: " + addr[0] + ":" + str(addr[1]) + "\n" +
+                             "File: " + temp_file_name[:-4] + "/" + file_path)
+
+                get_file_result = getFileFromZip(file_path, temp_file_name)
+
+                if not get_file_result:
+                    response = "RESPONSE: 301\r\n" \
+                               "FILESIZE: 0\r\n\r\n"
+                    conn.send(response.encode('utf8'))
+                    print("File " + temp_file_name[:-4] + "/" + file_path + " doesn't exist\n")
+                    logging.warning("File " + temp_file_name[:-4] + "/" + file_path + " doesn't exist")
+                    continue
+
+                response_header = "RESPONSE: 300\r\n" \
+                                  "FILESIZE: " + str(get_file_result["filesize"]) + "\r\n\r\n"
+                response = response_header.encode('utf8') + get_file_result["filecontent"]
+                try:
+                    conn.send(response)
+                except socket.error as socket_exc:
+                    print("Socket error: {0}".format(socket_exc))
+                    logging.error("Socket error: {0}".format(socket_exc))
                 else:
-                    try:
-                        print(file_content)
-                        file = open("uploaded_files/" + file_name, "wb+")
-                        file.write(file_content)
-                        file.close()
-                    except IOError as io_exc:
-                        print("I/O error: {0}".format(io_exc))
-                        response = "RESPONSE: 202\r\n\r\n"
-                        conn.send(response.encode())
-                        disconnectClient(conn, addr)
-                        return
-                    else:
-                        print("File: " + file_name + " has been successfully saved on the server")
-                        response = "RESPONSE: 200\r\n\r\n"
-                        conn.send(response.encode())
+                    print("File " + temp_file_name[:-4] + "/" + file_path + " successfully sent to client\n")
+                    logging.info("File " + temp_file_name[:-4] + "/" + file_path + " successfully sent to client")
+            else:
+                disconnectClient(conn, addr)
+                break
 
 
-server_address = ('127.0.0.1', 5000)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if __name__ == "__main__":
+    server_address = ('127.0.0.1', 5000)
+    ip_ver = ipVersion(server_address[0])
 
-try:
-    sock.bind(server_address)
-except socket.error as socket_exc:
-    print("Socket error: {0}".format(socket_exc))
-    exit(1)
-else:
-    sock.listen(5)
+    if ip_ver == 4:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif ip_ver == 6:
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    else:
+        print("Invalid ip address! The server can't be started")
+        exit(1)
 
-while True:
-    client, address = sock.accept()
-    print("Connected with: " + address[0])
-    t = threading.Thread(target=oneClient, args=[client, address])
-    t.start()
+    try:
+        print("Running the server...")
+        logging.info("Running the server...")
+        sock.bind(server_address)
+    except socket.error as socket_exc:
+        print("Socket error: {0}".format(socket_exc))
+        logging.error("Socket error: {0}".format(socket_exc))
+        exit(1)
+    else:
+        sock.listen(5)
+        print("Server is running on port 5000")
+        logging.info("Server is running on port 5000")
 
+    while True:
+        client, address = sock.accept()
+        print("Connected with: " + address[0] + ":" + str(address[1]))
+        logging.info("Connected with: " + address[0] + ":" + str(address[1]))
+        t = threading.Thread(target=oneClient, args=[client, address])
+        t.start()
 
